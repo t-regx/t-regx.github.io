8236fbad471bc13f035e28c0998ba44eb0c35a34
Daniel Wilkowski <daniel.wilkowski@gmail.com>
2018-10-24 17:10:25 +0200

    Temporarily hide API tab

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,7 +12,7 @@ const siteConfig = {
     headerLinks: [
         {search: true},
         {doc: 'installation', label: 'Docs'},
-        {doc: 'api/api', label: 'API'},
+        // {doc: 'api/api', label: 'API'},
         {page: 'help', label: 'Help'},
         {blog: true, label: 'Blog'},
         {href: 'https://github.com/T-Regx/T-Regx', label: 'GitHub'},
