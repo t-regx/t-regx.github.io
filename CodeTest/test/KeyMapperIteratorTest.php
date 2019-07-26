7b45512d3ad643c8262bb288c126bd43b193d200
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-07-27 01:42:52 +0200

    Add KeyMapperIterator

---
+++ CodeTest/test/KeyMapperIteratorTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace CodeTest;
+
+use ArrayIterator;
+use PHPUnit\Framework\TestCase;
+
+class KeyMapperIteratorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldMapKeys()
+    {
+        // given
+        $iterator = new KeyMapperIterator(new ArrayIterator([1, 2, 3]), function (int $key) {
+            return "key: $key";
+        });
+
+        // when
+        $result = iterator_to_array($iterator);
+
+        // then
+        $expected = [
+            'key: 0' => 1,
+            'key: 1' => 2,
+            'key: 2' => 3,
+        ];
+        $this->assertEquals($expected, $result);
+    }
+}
