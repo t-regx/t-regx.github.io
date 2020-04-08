04451530ea87cddbb18fa02c92c8de0c01444509
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-04-08 20:30:28 +0200

    Clean \pages\help\index.js

--- website/src/pages/help/index.js
+++ website/src/pages/help/index.js
@@ -1,45 +1,45 @@
 import React from 'react';
-import classnames from 'classnames';
-import Markdown from 'markdown-to-jsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import { DisqusThread } from '@site/src/components';
-import supportLinks from '@site/src/data/help.js';
-const issueLink = 'https://github.com/T-Regx/T-Regx/issues/new/choose';
+import Markdown from 'markdown-to-jsx';
+import classNames from 'classnames';
 
-function Help() {
+import DisqusThread from '../../components/DisqusThread';
+import supportLinks from '../../data/help.js';
+
+export default function Help() {
   return (
     <Layout>
       <div className="container margin-vert--xl">
         <div>
           <h1>Need help?</h1>
           <p>
-            This project is maintained by a dedicated group of people. If you
-            have experienced any bugs or problems, you're welcome to submit an{' '}
-            <a href={issueLink}>issue on github!</a> :) It never takes us more
-            than 24h to respond!
+            This project is maintained by a dedicated group of people. If you have experienced any bugs or problems,
+            you're welcome to submit an <IssueLink>issue on github!</IssueLink> :) It never takes us more than
+            24h to respond!
           </p>
-
           <div className="row">
-            {supportLinks.map(({ title, content }, idx) => (
-              <div
-                key={idx}
-                className={classnames('col margin-vert--md', {
-                  'col--4': idx <= 2,
-                  'col-6': idx > 2,
-                })}
-              >
-                <h2>{title}</h2>
-                <Markdown>{content}</Markdown>
-              </div>
-            ))}
+            <SupportLinks/>
           </div>
-
-          <DisqusThread />
+          <DisqusThread/>
         </div>
       </div>
     </Layout>
   );
 }
 
-export default Help;
+const IssueLink = ({children}) => {
+  const issueLink = 'https://github.com/T-Regx/T-Regx/issues/new/choose';
+  return <a href={issueLink}>{children}</a>
+};
+
+const SupportLinks = () => {
+  return supportLinks.map(({title, content}, idx) => (
+    <div key={idx} className={classNames('col margin-vert--md', {
+      'col--4': idx <= 2,
+      'col-6': idx > 2,
+    })}>
+      <h2>{title}</h2>
+      <Markdown>{content}</Markdown>
+    </div>
+  ));
+};
