1b254f88c220fb8849ef48fbde7e6531ed4b944f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-04-08 20:37:28 +0200

    Clean \pages\index\index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -11,6 +11,77 @@ import {AutomaticSplashLogo, GithubButton, DisqusThread, BadgesSection} from '..
 import sections from '../data/index.js';
 import styles from './styles.module.css';
 
+export default function Index() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+
+  return (
+    <Layout>
+      <HomeSplash
+        title={siteConfig.title}
+        tagline={siteConfig.tagline}
+        url={siteConfig.mainRepoUrl}/>
+      <div className="mainContainer">
+        <AutomaticDelimiters/>
+        <Installation/>
+        <MatchDetails/>
+        <Features/>
+        <WarningsToExceptions/>
+        <FunctionalProgramming/>
+        <CommentsSection/>
+      </div>
+    </Layout>
+  );
+}
+
+const HomeSplash = ({title, tagline, url}) => (
+  <SplashContainer>
+    <AutomaticSplashLogo/>
+    <ProjectTitle title={title} tagline={tagline}/>
+    <GithubButton href={url}/>
+    <PromoSection>
+      <HeaderButton to="docs/installation">Installation</HeaderButton>
+      <HeaderButton to="docs/match">Matching</HeaderButton>
+      <HeaderButton to="docs/replace-with">Replacing</HeaderButton>
+      <HeaderButton to="docs/handling-user-input">Prepared Patterns</HeaderButton>
+      <div className={styles.separator}/>
+      <HeaderButton to="docs/overview">What's T-Regx</HeaderButton>
+      <HeaderButton to="docs/whats-the-point">Why use T-Regx?</HeaderButton>
+    </PromoSection>
+  </SplashContainer>
+);
+
+const AutomaticDelimiters = () => <GridBlock
+  columns={sections.automaticDelimiters}
+  darkBackground scrollableBackground/>;
+
+const Installation = () => (
+  <GridBlock columns={sections.installation} center layout="threeColumn">
+    <BadgesSection/>
+  </GridBlock>
+);
+
+const MatchDetails = () => <GridBlock columns={sections.matchDetails} lightBackground/>;
+const Features = () => <GridBlock columns={sections.features} layout="fourColumn"/>;
+const FunctionalProgramming = () => <GridBlock columns={sections.functionalProgramming}/>;
+
+const WarningsToExceptions = () => (
+  <GridBlock
+    columns={sections.warningsToExceptions}
+    darkBackground
+    scrollableBackground
+  />
+);
+
+const CommentsSection = () => (
+  <div className="comments">
+    <div className="container">
+      <DisqusThread/>
+    </div>
+  </div>
+);
+
+
 const CustomCodeBlock = ({children, ...props}) => {
   return <CodeBlock {...children.props} />;
 };
@@ -48,51 +119,27 @@ const HeaderButton = ({to, children}) => (
   </Link>
 );
 
-const HomeSplash = ({title, tagline, url}) => (
-  <SplashContainer>
-    <AutomaticSplashLogo/>
-    <ProjectTitle title={title} tagline={tagline}/>
-    <GithubButton href={url}/>
-    <PromoSection>
-      <HeaderButton to="docs/installation">Installation</HeaderButton>
-      <HeaderButton to="docs/match">Matching</HeaderButton>
-      <HeaderButton to="docs/replace-with">Replacing</HeaderButton>
-      <HeaderButton to="docs/handling-user-input">
-        Prepared Patterns
-      </HeaderButton>
-      <div className={styles.separator}/>
-      <HeaderButton to="docs/overview">What's T-Regx</HeaderButton>
-      <HeaderButton to="docs/whats-the-point">Why use T-Regx?</HeaderButton>
-    </PromoSection>
-  </SplashContainer>
-);
-
 const GridBlock = props => (
-  <div
-    className={classNames('padding-vert--xl', {
-      [styles.scrollBackground]: props.scrollableBackground,
-      [styles.darkBackground]: props.darkBackground,
-      [styles.lightBackground]: props.lightBackground,
-      'text--center': props.center,
-    })}
-  >
+  <div className={classNames('padding-vert--xl', {
+    [styles.scrollBackground]: props.scrollableBackground,
+    [styles.darkBackground]: props.darkBackground,
+    [styles.lightBackground]: props.lightBackground,
+    'text--center': props.center,
+  })}>
     <div className="container">
       {props.children}
-      <div className="row ">
+      <div className="row">
         {props.columns.map((column, index) => (
           <div key={index} className="col">
-            {column.title && (
-              <h2>
-                <Markdown>{column.title}</Markdown>
-              </h2>
-            )}
+            {column.title && <h2>
+              <Markdown>{column.title}</Markdown>
+            </h2>}
 
-            {column.content && (
-              <div>
-                <Markdown options={{overrides: {pre: CustomCodeBlock,},}}>
-                  {column.content}
-                </Markdown>
-              </div>)}
+            {column.content && <div>
+              <Markdown options={{overrides: {pre: CustomCodeBlock,},}}>
+                {column.content}
+              </Markdown>
+            </div>}
 
             {column.video && (<video src={column.video} playsInline autoPlay muted loop style={{maxWidth: '100%'}}/>)}
           </div>
@@ -101,69 +148,3 @@ const GridBlock = props => (
     </div>
   </div>
 );
-
-const CommentsSection = props => (
-  <div className="comments">
-    <div className="container">
-      <DisqusThread/>
-    </div>
-  </div>
-);
-
-const FunctionalProgramming = () => (
-  <GridBlock columns={sections.functionalProgramming}/>
-);
-
-const MatchDetails = () => (
-  <GridBlock columns={sections.matchDetails} lightBackground/>
-);
-
-const Features = () => (
-  <GridBlock columns={sections.features} layout="fourColumn"/>
-);
-
-const Installation = () => (
-  <GridBlock columns={sections.installation} center layout="threeColumn">
-    <BadgesSection/>
-  </GridBlock>
-);
-
-const AutomaticDelimiters = () => (
-  <GridBlock
-    columns={sections.automaticDelimiters}
-    darkBackground
-    scrollableBackground
-  />
-);
-
-const WarningsToExceptions = () => (
-  <GridBlock
-    columns={sections.warningsToExceptions}
-    darkBackground
-    scrollableBackground
-  />
-);
-
-export default function Index() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-
-  return (
-    <Layout>
-      <HomeSplash
-        title={siteConfig.title}
-        tagline={siteConfig.tagline}
-        url={siteConfig.mainRepoUrl}
-      />
-      <div className="mainContainer">
-        <AutomaticDelimiters/>
-        <Installation/>
-        <MatchDetails/>
-        <Features/>
-        <WarningsToExceptions/>
-        <FunctionalProgramming/>
-        <CommentsSection/>
-      </div>
-    </Layout>
-  );
-}
