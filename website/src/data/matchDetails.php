9c0b37fa7063f0c033709a16ef0ffcfe71eacb63
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-09-11 00:36:06 +0200

    Clean matchDetails.php

--- website/src/data/matchDetails.php
+++ website/src/data/matchDetails.php
@@ -4,17 +4,18 @@ use TRegx\CleanRegex\Match\Details\Match;
 
 pattern('https?://(\w+\.\w+)')->match($string)->first(function (Match $match) {
     // cast to string
+    // highlight-next-line
     echo "I matched: $match";
 
     // capturing group
-// highlight-next-line
+    // highlight-next-line
     $domain = $match->get(1);
 
     // use offset (UTF-8 safe)
-// highlight-next-line
+    // highlight-next-line
     mb_substr($match, 0, $match->group(1)->offset());
 
     // use offset (bytes)
-// highlight-next-line
+    // highlight-next-line
     substr($match, 0, $match->group(1)->byteOffset());
 });
