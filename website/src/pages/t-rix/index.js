54be245ef6f28d42905cbcf44c0b5dee5640fec8
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-01-13 22:42:15 +0100

    Update /t-rix page

--- website/src/pages/t-rix/index.js
+++ website/src/pages/t-rix/index.js
@@ -2,23 +2,40 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Markdown from 'markdown-to-jsx';
 
+import tRegxMatrix from "../../../static/img/t.regx.matrix.png";
+
+import styles from "./index.module.scss";
+import DisqusThread from "../../components/DisqusThread";
+
 export default function MatrixPage() {
-  return (
+  return <div className={styles.matrixBackground}>
     <Layout>
       <div className="container margin-vert--xl">
-        <div className="post">
-          <img src="/img/blue-matrix-wide.jpg" alt="Matrix code"/>
+        <div className={styles.post}>
+          <img src={tRegxMatrix} alt="T-Regx in the dream world"/>
+          <Markdown children={`
+> Have you ever had a dream, that you were so sure was real? What if
+you were unable to wake from that dream? How would you know the
+difference between the dream world and the real world?
+`}/>
         </div>
-        <br/>
-        <div className="post" style={{display: 'flex'}}>
-          <img src="/img/t.regx.matrix.png" alt="T-Regx in the dream world"/>
-          <Markdown>
-            > Have you ever had a dream, that you were so sure was real? What if
-            you were unable to wake from that dream? How would you know the
-            difference between the dream world and the real world?
-          </Markdown>
+        <div style={{marginBottom: '30px'}}>
+          <YouTubeEmbedded/>
+        </div>
+        <div className={styles.commentsBackground}>
+        <DisqusThread
+          title={'T-Regx Matrix'}
+          identifier={'34641aa1c25319a367666d815a258466'}/>
         </div>
       </div>
     </Layout>
-  );
+  </div>;
+}
+
+function YouTubeEmbedded() {
+  return <iframe
+    width="560" height="315"
+    src="https://www.youtube.com/embed/G7RgN9ijwE4"
+    frameBorder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>;
 }
