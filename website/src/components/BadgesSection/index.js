3f5bd5a3e69b6a0c332b2158a38954483cb7d454
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-02-16 23:21:17 +0100

    Move badges.js to config/

--- website/src/components/BadgesSection/index.js
+++ website/src/components/BadgesSection/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
 
-import badges from '../../data/badges';
-import styles from './styles.module.css';
 import StarButton from "../Github/StarButton";
+
+import badges from '../../config/badges';
+import styles from './styles.module.css';
 
 export default function BadgesSection() {
   const {upper: upperBadges, lower: lowerBadges} = badges;
 
-  return (
-    <div className={styles.badgesSection}>
-      <div className={styles.upperBadges}>
-        {upperBadges.map((badge, index) => (
-          <Badge key={index} {...badge} />
-        ))}
-      </div>
-      <div className={styles.lowerBadges}>
-        {lowerBadges.map((badge, index) => (
-          <Badge key={index} {...badge} />
-        ))}
-        <StarButton/>
-      </div>
+  return <div className={styles.badgesSection}>
+    <div className={styles.upperBadges}>
+      {upperBadges.map((badge, index) => (
+        <Badge key={index} {...badge} />
+      ))}
     </div>
-  );
+    <div className={styles.lowerBadges}>
+      {lowerBadges.map((badge, index) => (
+        <Badge key={index} {...badge} />
+      ))}
+      <StarButton/>
+    </div>
+  </div>;
 };
 
 const Badge = ({title, href, src}) => (
