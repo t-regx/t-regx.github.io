28342889dbe43671cf926dbb00d37fcafc1a6a1a
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-02-16 18:12:21 +0100

    Move <Link/> to components/Link/

---
+++ website/src/components/Link/Link.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import Link from "@docusaurus/Link";
+
+import {tryItOnline} from '../../data/links';
+
+export default ({to, children}: LinkProperties) => {
+  if (to) {
+    return <Link to={to}><code>{children}</code></Link>
+  }
+  const {link, content} = linkAndText(children);
+  if (link) {
+    return <Link to={link} children={content}/>
+  }
+  return content;
+};
+
+function linkAndText(key: string): {link?: string, content} {
+  if (codeLinks.hasOwnProperty(key)) {
+    return {content: <code>{key}</code>, link: codeLinks[key]};
+  }
+  if (textLinks.hasOwnProperty(key)) {
+    return {content: key, link: textLinks[key]};
+  }
+  if (elementLinks.hasOwnProperty(key)) {
+    return elementLinks[key];
+  }
+  console.log(`No default link for '${key}'`);
+  return {content: <code>{key}</code>};
+}
+
+const elementLinks: {[key: string]: {content: string, link: string}} = {
+  'pattern().match()': {content: 'pattern()->match()', link: 'docs/match'},
+  'pattern().replace()': {content: 'pattern()->replace()', link: 'docs/replace'},
+};
+
+const codeLinks = {
+  'pattern()': 'docs/introduction-clean#entry-points',
+  'Pattern::of()': 'docs/introduction-clean#entry-points',
+  'Pattern::pcre()': 'docs/introduction-clean#entry-points',
+  'Pattern::inject()': 'docs/handling-user-input',
+  'Pattern::bind()': 'docs/handling-user-input',
+
+  'Detail': 'docs/match-details',
+};
+
+const textLinks = {
+  'try it online': tryItOnline,
+  'sandbox': tryItOnline,
+  'T-Regx on repl.it': tryItOnline,
+  'prepared patterns': 'docs/handling-user-input',
+  'Prepared Patterns': 'docs/handling-user-input',
+};
+
+interface LinkProperties {
+  to?: string,
+  children: string
+}
