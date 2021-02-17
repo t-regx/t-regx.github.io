bcc5f2aad85979aa5554ce8387a24d70795dd819
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-02-17 20:17:17 +0100

    Extract <PregSuperposition/>

---
+++ website/src/components/PregSuperposition/PregSuperposition.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TextSuperposition from "../../components/TextSuperposition/TextSuperposition";
+
+import {pregDocs} from "../../config/links";
+import Link from "../../components/Link/Link";
+
+import {wide, wider, narrow} from "./styles.module.scss";
+
+export default function () {
+  const pregMethods = [
+    <code className={wide}>preg_match()</code>,
+    <code className={narrow}>preg_match_all()</code>,
+    <code >preg_replace()</code>,
+    <code className={wide}>preg_split()</code>,
+    <code className={wider}>preg_grep()</code>,
+    <code>preg_filter()</code>,
+  ];
+
+  return <Link to={pregDocs}><TextSuperposition texts={pregMethods}/></Link>;
+}
