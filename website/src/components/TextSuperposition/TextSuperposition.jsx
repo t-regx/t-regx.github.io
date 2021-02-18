5f41edeb5e66699c6b673d62632db8989bfdc7cb
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-02-18 21:20:11 +0100

    Make <TextSuperposition/> stop superposition on mouse hover

--- website/src/components/TextSuperposition/TextSuperposition.jsx
+++ website/src/components/TextSuperposition/TextSuperposition.jsx
@@ -1,30 +1,67 @@
-import React, {useEffect, useState} from "react";
+import React, {Component} from "react";
 
 import styles from "./style.module.scss";
 
-export default ({texts}) => {
-  const items = [...texts, texts[0]];
-
-  const [slide, setSlide] = useState(0);
-
-  function nextSlide(s) {
-    const newSlide = (s + 1) % items.length;
-    if (newSlide === items.length - 1) {
-      setTimeout(() => setSlide(0), 500);
-    }
-    return newSlide;
+class Stopwatch {
+  constructor(method, delay) {
+    this.method = method;
+    this.delay = delay;
+    this.interval = null;
   }
 
-  useEffect(() => {
-    const interval = setInterval(() => setSlide(s => nextSlide(s)), 1000);
-    return () => clearInterval(interval);
-  });
+  start() {
+    if (this.interval === null) {
+      this.interval = setInterval(this.method, this.delay);
+    }
+  }
 
-  return <span className={styles.parent}>
-    <span className={styles.container} style={style(slide)}>
-      {items.map((item, index) => <span key={index} children={item}/>)}
-    </span>
-  </span>;
+  stop() {
+    if (this.interval !== null) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+}
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    const {texts} = props;
+    if (!texts || texts.length === 0) {
+      throw "TextSuperposition doesn't make sense without values";
+    }
+    this.state = {
+      items: [...texts, texts[0]],
+      slide: 0,
+    };
+    this.stopwatch = new Stopwatch(() => this.nextSlide(), 1000)
+  }
+
+  componentDidMount() {
+    this.stopwatch.start();
+  }
+
+  componentWillUnmount() {
+    this.stopwatch.stop();
+  }
+
+  nextSlide() {
+    const newSlide = (this.state.slide + 1) % this.state.items.length;
+    if (newSlide === this.state.items.length - 1) {
+      setTimeout(() => this.setState({slide: 0}), 500);
+    }
+    return this.setState({slide: newSlide});
+  }
+
+  render() {
+    return <span className={styles.parent}
+                 onMouseEnter={() => this.stopwatch.stop()}
+                 onMouseLeave={() => this.stopwatch.start()}>
+      <span className={styles.container} style={style(this.state.slide)}>
+        {this.state.items.map((item, index) => <span key={index} children={item}/>)}
+      </span>
+    </span>;
+  }
 }
 
 function style(slide) {
