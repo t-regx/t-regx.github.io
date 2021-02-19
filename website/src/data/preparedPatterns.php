206dfd5788ea7540041c63f71fdc885cf4e505a8
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-02-19 21:25:56 +0100

    Update preparedPatterns.php

--- website/src/data/preparedPatterns.php
+++ website/src/data/preparedPatterns.php
@@ -1,19 +1,17 @@
 <?php
 
 use TRegx\CleanRegex\Match\Details\Detail;
-use TRegx\CleanRegex\Match\Details\NotMatched;
 use TRegx\CleanRegex\Pattern;
 
-$domain = $_GET['domain'];
+$link = $_GET['link'];
 
 // highlight-next-line
-Pattern::inject('(?<scheme>https?)://(?<uri>@/[^ ]+)', [$domain])
-    ->match($message)
-    ->findFirst(function (Detail $detail) {
-        $scheme = $detail->get('scheme');
-        $uri = $detail->get('uri');
-    })
-    ->orElse(function (NotMatched $notMatched) {
-        $message = $notMatched->subject();
-        throw new Exception("Sorry! Not found in $message!");
-    });
+Pattern::inject('(?<scheme>https?)://(?<domain>@/[^ ]+)', [$link])
+  ->match($message)
+  ->first(function (Detail $detail) {
+      // scheme
+      $scheme = $detail->get('scheme');
+
+      // domain
+      $domain = $detail->get('domain');
+  });
