345c98e64740bba8f5017bfeed60dfd66af800cf
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-10-11 04:54:17 +0200

    "Tutorial" in header points to docs/overview

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -30,7 +30,7 @@ module.exports = {
         src: 'img/t.regx.borderless.png',
       },
       items: [
-        {position: 'left', label: 'Tutorial', to: 'docs/installation'},
+        {position: 'left', label: 'Tutorial', to: 'docs/overview'},
         {position: 'left', label: 'Blog', to: 'blog'},
         {position: 'left', label: 'Compare', to: 'comparison'},
         {position: 'left', label: 'Help', to: 'help'},
